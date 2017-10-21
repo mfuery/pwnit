e33c2445eab128e4efea4d5abcea72e94a2c9016
Julian Threatt <porfirio84@gmail.com>
2017-10-21 15:52:28 -0700

    initial commit

---
+++ frontend/static/webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack');
+var path = require('path');
+var BundleTracker = require('webpack-bundle-tracker');
+
+var BUILD_DIR = path.resolve(__dirname, 'js');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+var config = {
+  context: __dirname,
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+	loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new BundleTracker({filename: './webpack-stats.json'})
+  ]
+};
+
+module.exports = config;
