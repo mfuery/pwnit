fa884282870fbb33c5fc173550f4d54d114cc3ad
jkang8 <johnkang.h@gmail.com>
2017-10-21 20:08:05 -0700

    item model, django graphql setup

---
+++ pwnit/schema.py
@@ -0,0 +1,13 @@
+import graphene
+
+# import ingredients.schema
+import items.schema
+
+
+class Query(items.schema.Query, graphene.ObjectType):
+    # This class will inherit from multiple Queries
+    # as we begin to add more apps to our project
+    pass
+
+
+schema = graphene.Schema(query=Query)
