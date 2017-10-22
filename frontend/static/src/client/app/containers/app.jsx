244e6d44cf08e7ca08a079f9154dcf95bde9bf24
jkang8 <johnkang.h@gmail.com>
2017-10-21 21:33:06 -0700

    commit templates

--- frontend/static/src/client/app/containers/app.jsx
+++ frontend/static/src/client/app/containers/app.jsx
@@ -36,24 +36,102 @@ class Home extends React.Component {
     }
 }
 
+class ItemDetail extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { pageState: 'default' };
+    }
+    setPageState(pageType) {
+        this.setState({pageState: pageType});
+    }
+    renderButtons() {
+        let pageState = this.state.pageState;
+        switch(pageState) {
+            case 'counter':
+                return (<div>
+                    <button onClick={e => {
+                        this.setPageState('back');
+                    }}>Go Back</button>
+                    <input
+                        placeholder="Enter your counter offer">
+                    </input>
+                </div>);
+            case 'match':
+                return (<div>
+                    <button onClick={e => {
+                        this.setPageState('back');
+                    }}>Go Back</button>
+                    <button onClick={e => {
+                        this.setPageState('success');
+                    }}>Confirm</button>
+                </div>);
+            case 'success':
+                return (<div>
+                    <button onClick={e => {
+                        this.setPageState('get_key');
+                    }}>Get Key</button>
+                    <button onClick={e => {
+                        this.setPageState('confirm');
+                    }}>Profile</button>
+                </div>);
+            default:
+                return (<div>
+                    <button onClick={e => {
+                        this.setPageState('back');
+                    }}>Go Back</button>
+                    <button onClick={e => {
+                        this.setPageState('counter');
+                    }}>Counter</button>
+                    <button onClick={e => {
+                        this.setPageState('match');
+                    }}>Match Offer</button>
+                </div>)
+
+        };
+        // return <div>
+        //     <button onClick={e => {
+        //         this.setPageState('back');
+        //     }}>Go Back</button>
+        //     <button onClick={e => {
+        //         this.setPageState('counter');
+        //     }}>Counter</button>
+        //     <button onClick={e => {
+        //         this.setPageState('match');
+        //     }}>Match Offer</button>
+        // </div>
+    }
+    render () {
+        const item = this.props.item;
+        return <div>
+            <h1>{`${item.name} offered by Arthas`}</h1>
+            {this.renderButtons()}
+            <div>{item.description}</div>
+        </div>;
+    }
+}
+
+
 class Card extends React.Component {
     render() {
         return (<div className="card">
             <div className="card-name">
-            {this.props.name}
+                {this.props.name}
             </div>
             <div className="card-desc">
-            {this.props.description}
+                {this.props.description}
             </div>
         </div>);
     }
 }
 
 export default class App extends React.Component {
-  render () {
-    return (<div className="app-container">
-        <Home/>
-    </div>);
-  }
+    render () {
+        return (<div className="app-container">
+            <Home/>
+            {/*<ItemDetail*/}
+                {/*item={{name: 'lol', description: 'hello world'}}*/}
+            {/*/>*/}
+        </div>);
+    }
 }
 
